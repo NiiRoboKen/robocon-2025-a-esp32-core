567d6f05651c2aeef4b9cdfb0a2eef6def22c2f2
Sootta <soutaaibara@gmail.com>
2025-09-28 13:07:39 +0900

    add:Serial.begin()

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,16 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+const int8_t SERIAL1_RX_PIN = 4;
+const int8_t SERIAL1_TX_PIN = 5;
+const int8_t SERIAL2_RX_PIN = 12;
+const int8_t SERIAL2_TX_PIN = 13;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  Serial1.begin(115200, SERIAL_8N1, SERIAL1_RX_PIN, SERIAL1_TX_PIN);
+  Serial2.begin(115200, SERIAL_8N1, SERIAL2_RX_PIN, SERIAL2_TX_PIN);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
